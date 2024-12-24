1a9a9de51f7ace2656041da7a5751713302cd764
Jon Rafkind <jon@rafkind.com>
2024-12-23 20:14:26 -0800

    add all instruction encodings

--- core/cpu.go
+++ core/cpu.go
@@ -44,14 +44,96 @@ func DecodeInstruction(instruction byte) string {
             }
 
             switch instruction & 0b111 {
+                case 0b000:
+                    if instruction == 0b00011000 {
+                        return "jr imm8"
+                    }
+
+                    if instruction == 0b00010000 {
+                        return "stop"
+                    }
+
+                    if instruction >> 5 == 0b001 {
+                        return "jr cond, imm8"
+                    }
+
                 case 0b100: return "inc r8"
                 case 0b101: return "dec r8"
                 case 0b110: return "ld r8, imm8"
             }
 
         case 0b01:
+            if instruction & 0b11111 == 0b110110 {
+                return "halt"
+            }
+
+            // source := instruction & 0b111
+            // dest := (instruction >> 3) & 0b111
+
+            return "ld r8, r8"
+
         case 0b10:
+            switch (instruction >> 3) & 0b111 {
+                case 0b000: return "add a, r8"
+                case 0b001: return "adc a, r8"
+                case 0b010: return "sub a, r8"
+                case 0b011: return "sbc a, r8"
+                case 0b100: return "and a, r8"
+                case 0b101: return "xor a, r8"
+                case 0b110: return "or a, r8"
+                case 0b111: return "cp a, r8"
+            }
         case 0b11:
+            switch instruction & 0b111111 {
+                case 0b000110: return "add a, imm8"
+                case 0b001110: return "adc a, imm8"
+                case 0b010110: return "sub a, imm8"
+                case 0b011110: return "sbc a, imm8"
+                case 0b100110: return "and a, imm8"
+                case 0b101110: return "xor a, imm8"
+                case 0b110110: return "or a, imm8"
+                case 0b111110: return "cp a, imm8"
+
+                case 0b100010: return "ldh [c], a"
+                case 0b100000: return "ldh [imm8], a"
+                case 0b101010: return "ld [imm16], a"
+                case 0b110010: return "ldh a, [c]"
+                case 0b110000: return "ldh a, [imm8]"
+                case 0b111010: return "ld a, [imm16]"
+
+                case 0b101000: return "add sp, imm8"
+                case 0b111000: return "ld hl, sp + imm8"
+                case 0b111001: return "ld sp, hl"
+
+                case 0b110011: return "di"
+                case 0b111011: return "ei"
+            }
+
+            switch instruction & 0b1111 {
+                case 0b0001: return "pop r16stk"
+                case 0b0101: return "push r16stk"
+            }
+
+            if instruction >> 5 == 0b110 {
+                switch instruction & 0b111 {
+                    case 0b000: return "ret cond"
+                    case 0b010: return "jp cond, imm16"
+                    case 0b011: return "jp imm16"
+                    case 0b100: return "call cond, imm16"
+                    case 0b111: return "rst tgt3"
+                }
+
+                switch instruction {
+                    case 0b11001001: return "ret"
+                    case 0b11011001: return "reti"
+                    case 0b11101001: return "jp hl"
+                    case 0b11001101: return "call imm16"
+                }
+            }
+
+            if instruction == 0xcb {
+                // special prefix instruction
+            }
     }
 
     return "unknown"
