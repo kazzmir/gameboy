e247ae82602e9797a60b37f89aabe96309980bc1
Jon Rafkind <jon@rafkind.com>
2024-12-24 17:47:36 -0800

    ld r16, imm16

--- core/cpu.go
+++ core/cpu.go
@@ -23,7 +23,20 @@ const (
     Nop Opcode = iota
     RRCA
 
+    LoadBCImmediate
+    LoadDEImmediate
+    LoadHLImmediate
+    LoadSPImmediate
+
     Unknown
+)
+
+type R16 uint8
+const (
+    R16BC R16 = 0
+    R16DE R16 = 1
+    R16HL R16 = 2
+    R16SP R16 = 3
 )
 
 type Instruction struct {
@@ -52,9 +65,36 @@ func (cpu *CPU) Execute(instruction Instruction) {
             newA, carry := RotateRight(cpu.A)
             cpu.A = newA
             cpu.F = carryFlag(carry)
+        case LoadBCImmediate:
+            cpu.Cycles += 3
+            cpu.BC = instruction.Immediate16
+        case LoadDEImmediate:
+            cpu.Cycles += 3
+            cpu.DE = instruction.Immediate16
+        case LoadHLImmediate:
+            cpu.Cycles += 3
+            cpu.HL = instruction.Immediate16
+        case LoadSPImmediate:
+            cpu.Cycles += 3
+            cpu.SP = instruction.Immediate16
         default:
             log.Printf("Execute error: unknown opcode %v", instruction.Opcode)
     }
+}
+
+func makeImm16(data []byte) uint16 {
+    return uint16(data[0]) | (uint16(data[1]) << 8)
+}
+
+func makeLoadR16Imm16Instruction(r16 R16, immediate uint16) Instruction {
+    switch r16 {
+        case R16BC: return Instruction{Opcode: LoadBCImmediate, Immediate16: immediate}
+        case R16DE: return Instruction{Opcode: LoadDEImmediate, Immediate16: immediate}
+        case R16HL: return Instruction{Opcode: LoadHLImmediate, Immediate16: immediate}
+        case R16SP: return Instruction{Opcode: LoadSPImmediate, Immediate16: immediate}
+    }
+
+    return Instruction{Opcode: Unknown}
 }
 
 // instructions should be at least 3 bytes long for 'opcode immediate immediate'
@@ -66,8 +106,10 @@ func DecodeInstruction(instructions []byte) (Instruction, uint8) {
         case 0b00:
             switch instruction & 0b1111 {
                 case 0b0000: return Instruction{Opcode: Nop}, 1
+                case 0b0001:
+                    r16 := (instruction >> 4) & 0b11
+                    return makeLoadR16Imm16Instruction(R16(r16), makeImm16(instructions[1:])), 3
                 /*
-                case 0b0001: return "ld r16, imm16"
                 case 0b0010: return "ld [r16mem], a"
                 case 0b1010: return "ld a, [r16mem]"
                 case 0b1000: return "ld [imm16], sp"
