20af7767a85729cff9060174c196af0b423609ce
Jon Rafkind <jon@rafkind.com>
2025-06-03 20:56:42 -0700

    implement basic mbc2

--- core/mbc.go
+++ core/mbc.go
@@ -206,8 +206,67 @@ func (mbc3 *MBC3) Write(address uint16, value uint8) {
     }
 }
 
+type MBC2 struct {
+    rom []uint8
+    ram []uint8
+    romBank uint8
+    ramEnable bool
+}
+
+func (mbc2 *MBC2) Read(address uint16) uint8 {
+    switch {
+        case address < 0x4000:
+            return mbc2.rom[address]
+        case address >= 0x4000 && address < 0x8000:
+            address2 := (uint32(mbc2.romBank) << 14) | uint32(address & 0b11_1111_1111_1111)
+            if address2 >= uint32(len(mbc2.rom)) {
+                log.Printf("Attempted to read from ROM at address 0x%x", address)
+                return 0
+            }
+            // log.Printf("mbc1 read 0x4000 range address=0x%x, romBank=%d, ramBank=%d, address2=0x%x value=0x%x", address, mbc1.romBank, mbc1.ramBank, address2, mbc1.rom[address2])
+            return mbc2.rom[address2]
+        case address >= 0xa000 && address < 0xc000:
+            if mbc2.ramEnable {
+                address2 := address & 0b1_1111_1111
+                if address2 >= uint16(len(mbc2.ram)) {
+                    log.Printf("Attempted to read from RAM at address 0x%x", address)
+                    return 0
+                }
+                return mbc2.ram[address2]
+            }
+
+    }
+
+    log.Printf("Warning: mbc2: read from unsupported address 0x%x", address)
+
+    return 0
+}
+
+func (mbc2 *MBC2) Write(address uint16, value uint8) {
+    switch {
+        case address < 0x4000:
+            if value & 0b1111 == 0xa {
+                mbc2.ramEnable = true
+            } else {
+                mbc2.ramEnable = false
+            }
+        case address >= 0xa000 && address < 0xc000:
+            if mbc2.ramEnable {
+                address2 := address & 0b1_1111_1111
+                if address2 >= uint16(len(mbc2.ram)) {
+                    log.Printf("Attempted to write to RAM at address 0x%x", address)
+                } else {
+                    mbc2.ram[address2] = value
+                }
+            }
+        default:
+            log.Printf("MBC2: unhandled write to address 0x%x: 0x%x", address, value)
+    }
+}
+
 var _ MBC = &MBC0{}
 var _ MBC = &MBC1{}
+var _ MBC = &MBC2{}
 var _ MBC = &MBC3{}
 
 func MakeMBC(mbcType uint8, rom []uint8) (MBC, error) {
@@ -226,6 +285,11 @@ func MakeMBC(mbcType uint8, rom []uint8) (MBC, error) {
                 rom: rom,
                 ram: make([]uint8, 0x2000 * 8), // 8 banks of 8k each
                 rtcValues: make([]uint8, 5), // 5 RTC values
+            }, nil
+        case 0x5, 0x6:
+            return &MBC2{
+                rom: rom,
+                ram: make([]uint8, 512), // MBC2 has 512 bytes of RAM
             }, nil
         default:
             return nil, fmt.Errorf("Unknown MBC type")
